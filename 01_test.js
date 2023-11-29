0970b435ae8c141b485a10af7ab5cfe941d88c90
Umair <47umair0101@gmail.com>
2023-11-29 19:04:12 +0500

    first commit

---
+++ 01_test.js
@@ -0,0 +1,6 @@
+const accountId = 470101;
+const accountName = "Umair";
+const accountEmail = "umair@gmail.com";
+const accountPassword = "hello!";
+
+console.table([accountId, accountName, accountEmail, accountPassword]);
